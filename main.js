c0d3d2e2a92eae4b4be27e5f7164686a99a7113f
ramizMahmmud <89058495+ramizMahmmud@users.noreply.github.com>
2023-09-13 11:02:58 +0600

    ADd dom
    button is added and event listener is added. but there is same kind of function thrice. it need to be one and loop through it.further modification needed

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function getComputerChoice(){
+function getComputerChoice() {
     let computerChoice;
-    let x = (Math.random() * 10) %3;
+    let x = (Math.random() * 10) % 3;
     x = Math.floor(x);
-    switch(x){
+    switch (x) {
         case 0:
             computerChoice = "rock";
             break;
@@ -15,68 +15,107 @@ function getComputerChoice(){
     }
     return computerChoice;
 }
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection, computerSelection) {
 
-    if(playerSelection === "rock" && computerSelection ==="rock"){
+
+    if (playerSelection === "rock" && computerSelection === "rock") {
         return "Tie";
     }
-    else if(playerSelection === "rock" && computerSelection ==="paper"){
+    else if (playerSelection === "rock" && computerSelection === "paper") {
         return "You Lose! Paper beats rocks";
     }
-    else if(playerSelection === "rock" && computerSelection ==="scissors"){
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You win! Rock beats scissors.";
     }
 
-    else if(playerSelection === "paper" && computerSelection ==="scissors"){
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
         return "You lose! scissors beats paper.";
     }
-    else if(playerSelection === "paper" && computerSelection ==="paper"){
+    else if (playerSelection === "paper" && computerSelection === "paper") {
         return "Tie";
     }
-    else if(playerSelection === "paper" && computerSelection ==="rock"){
+    else if (playerSelection === "paper" && computerSelection === "rock") {
         return "You win! Paper beats rock";
     }
 
- 
-    else if(playerSelection === "scissors" && computerSelection ==="paper"){
+
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
         return "You win! scissors beats paper.";
     }
-    else if(playerSelection === "scissors" && computerSelection ==="scissors"){
+    else if (playerSelection === "scissors" && computerSelection === "scissors") {
         return "Tie";
     }
-    else if(playerSelection === "scissors" && computerSelection ==="rock"){
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
         return "You lose! Rock beats scissors";
     }
 
-    else if(playerSelection === "paper" && computerSelection ==="scissors"){
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
         return "You lose! scissors beats paper.";
     }
-    else if(playerSelection === "paper" && computerSelection ==="paper"){
+    else if (playerSelection === "paper" && computerSelection === "paper") {
         return "Tie";
     }
-    else if(playerSelection === "paper" && computerSelection ==="rock"){
+    else if (playerSelection === "paper" && computerSelection === "rock") {
         return "You win! Paper beats rock";
     }
 
 }
-function fiveRoundGame(){
-    let yourWin = 0;
-    let computerWin = 0;
-    while(yourWin+computerWin<5){
-        const playerSelection = prompt("Enter rock or paper or scissors!");
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection,computerSelection);
-        console.log(result)
-        if(result === "Tie"){
-            continue;
-        }
-        else if(result.slice(0,8) ==="You win!"){
-            yourWin++;
-        }
-        else computerWin++;
-    }
-    console.log("Your win:" + yourWin + " and " + "Computer Win: " + computerWin);
-}
-fiveRoundGame()
+// function fiveRoundGame(){
+//     let yourWin = 0;
+//     let computerWin = 0;
+//     while(yourWin+computerWin<5){
+//         const playerSelection = prompt("Enter rock or paper or scissors!");
+//         const computerSelection = getComputerChoice();
+//         const result = playRound(playerSelection,computerSelection);
+//         console.log(result)
+//         if(result === "Tie"){
+//             continue;
+//         }
+//         else if(result.slice(0,8) ==="You win!"){
+//             yourWin++;
+//         }
+//         else computerWin++;
+//     }
+//     console.log("Your win:" + yourWin + " and " + "Computer Win: " + computerWin);
+// }
+// fiveRoundGame()
 
+function playRock() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = 'rock';
+    let message = playRound(playerChoice, computerChoice);
+    const result = document.querySelector('#result');
+    const show = document.createElement('div');
+    show.textContent = message;
+    result.appendChild(show);
+}
+
+function playPaper() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = 'paper';
+    let message = playRound(playerChoice, computerChoice);
+    const result = document.querySelector('#result');
+    const show = document.createElement('div');
+    show.textContent = message;
+    result.appendChild(show);
+}
+
+function palyScissors() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = 'scissors';
+    let message = playRound(playerChoice, computerChoice);
+    const result = document.querySelector('#result');
+    const show = document.createElement('div');
+    show.textContent = message;
+    result.appendChild(show);
+}
+
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', palyScissors);
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', playRock);
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', playPaper);
